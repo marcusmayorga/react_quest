70a15b2abe1b7cc17100957795ed1d4e6eb6fadb
Marcus Mayorga <mayorga.ml@gmail.com>
2024-09-23 21:53:06 -0700

    Added basic setup

---
+++ feedback-app/src/App.js
@@ -0,0 +1,24 @@
+function App() {
+  const title = 'Blog Post';
+  const body = 'This is my blog post';
+  const comments = [
+    {id: 1, text: 'Comment One'},
+    {id: 2, text: 'Comment Two'},
+    {id: 3, text: 'Comment Three'},
+  ]
+  return (
+    <div className='container'>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+      <div className='comments'>
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+            {comments.map((comment, index) => (
+                <li key={index}>{comment.text}</li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default App;
