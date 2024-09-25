c34a0eabc9c37fabf2969981f6dcb0396432911a
Marcus Mayorga <mayorga.ml@gmail.com>
2024-09-24 19:46:37 -0700

    Practice and start of feedback UI

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,25 +1,12 @@
+import Header from './components/Header';
 function App() {
-  const title = 'Blog Post';
-  const body = 'This is my blog post';
-  const comments = [
-    {id: 1, text: 'Comment One'},
-    {id: 2, text: 'Comment Two'},
-    {id: 3, text: 'Comment Three'},
-    {id: 4, text: 'Comment Four'}
-  ]
   return (
-    <div className='container'>
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-      <div className='comments'>
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-            {comments.map((comment, index) => (
-                <li key={index}>{comment.text}</li>
-            ))}
-        </ul>
+    <>
+      <Header />
+      <div className='container'>
+        <h1>My App</h1>
       </div>
-    </div>
+    </>
   );
 }
 export default App;
